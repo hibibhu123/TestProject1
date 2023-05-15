5d26d4aa93c71931c7401ab25fb771ab2433cf28
hibibhu123 <hibibhu123@gmail.com>
2023-05-15 18:46:48 +0530

    2nd commit

--- src/hello1/FirstPro1.java
+++ src/hello1/FirstPro1.java
@@ -4,7 +4,7 @@ public class FirstPro1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("***************************");
+		System.out.println("Test");
 	}
 
 }
